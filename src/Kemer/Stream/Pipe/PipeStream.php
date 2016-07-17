05cc9c6d6331f473571b97ca562513e5d741996e
Tomasz Jonik <tom.jonik@hotmail.com>
2016-07-17 01:56:38 +0100

    Send error output to php://stderr

--- src/Kemer/Stream/Pipe/PipeStream.php
+++ src/Kemer/Stream/Pipe/PipeStream.php
@@ -97,7 +97,7 @@ class PipeStream extends StreamWrapper
         $descriptorspec = array(
            0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
            1 => array("pipe", "w"), //$stream,  // stdout is a pipe that the child will write to
-           2 => array("file", "/tmp/error-output-pipe.txt", "a") // stderr is a file to write to
+           2 => array("file", "php://stderr", "w") // stderr is a file to write to
         );
 
         $this->process = proc_open($this->command, $descriptorspec, $pipes);
